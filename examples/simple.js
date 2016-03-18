b18bda438fa181f75aa2804de7baea8e94a14a39
shizle <gastonnkh@gmail.com>
2016-03-18 21:06:50 +0100

    corrections to the bar charts lengths for various expressions levels

--- examples/simple.js
+++ examples/simple.js
@@ -1,5 +1,5 @@
 // if you don't specify a html file, the sniper will generate a div with id "rootDiv"
-var nextprot = require("nextprot-expression-viewer");
+var nextprot = require("expression-viewer");
 var viewer = new nextprot({el: rootDiv});
 
 viewer.render('NX_P01308');
