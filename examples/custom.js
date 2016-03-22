974707348459f544722409d8864ecbf44769c283
shizle <gastonnkh@gmail.com>
2016-03-22 06:01:09 +0100

    making the package more customizable

---
+++ examples/custom.js
@@ -0,0 +1,9 @@
+colors={
+	positive:"orange",
+	high:"green",
+	medium:"purple",
+	low:"lightblue",
+	negative:"black"
+	}
+var viewer = require('expression-viewer');
+var next = new viewer({el: rootDiv,protein:'NX_P01308',colors:colors});
